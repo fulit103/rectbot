748432bd04a4348a0bf53219f7984b82dc3036e6
Julian Toro <jtoro@tecmovin.com>
2019-12-16 12:27:16 -0500

    settings

--- chatbot/chatbot.js
+++ chatbot/chatbot.js
@@ -13,7 +13,7 @@ const credentials = {
 
 const sessionClient = new dialogflow.SessionsClient({projectID: projectID, credentials: credentials});
 
-const sessionPath = sessionClient.sessionPath(config.googleProjectID, config.dialogFlowSessionId);
+const sessionPath = sessionClient.sessionPath(config.googleProjectID, config.dialogFlowSessionID);
 
 module.exports = {
   textQuery: async function (text, parameters = {}){
